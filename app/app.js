1e8b341b69f37dd0647ef307ff8509d8f3b06a22
Bernard Anderson <bernard.anderson@gmail.com>
2016-06-03 17:55:12 -0500

    Added the routing for the various pages

---
+++ app/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var app = angular.module("MovieHistory", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider.
+    when("/", {
+      templateUrl: "partials/movieCard.html",
+      controller: "WatchedListCtrl"
+    }).
+    when("/watched", {
+      templateUrl: "partials/movieCard.html",
+      controller: "WatchedCtrl"
+    }).
+    when("/register", {
+      templateUrl: "partials/login.html",
+      controller: "LoginCtrl"
+    }).
+    when("/login", {
+      templateUrl: "partials/login.html",
+      controller: "LoginCtrl"
+    }).
+    when("/logout", {
+      templateUrl: "partials/login.html",
+      controller: "LoginCtrl"
+    }).
+    otherwise("/");.
+});
