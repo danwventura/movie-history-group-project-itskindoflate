992ca40deb17a8ee9ab6057c0f00f538d28cedb5
Dan Ventura <danwventura@icloud.com>
2016-06-03 20:06:58 -0500

    adding what we have after peer code on friday

---
+++ app/controllers/SeenCtrl.js
@@ -0,0 +1,6 @@
+app.controller("SeenCtrl", function(){
+
+
+
+
+});
--- app/factories/OMDBFactory.js
+++ app/factories/OMDBFactory.js
@@ -2,28 +2,28 @@
 
 app.factory("omdbFactory", function($q, $http){
 
-
-  //Function to grab data from (json) OMDB API
-  var getMoviesFromApi = function(){
-    let movies = [];
-    return $q(function(resolve, reject){
-      $http.get(`./data/movies.json`)
-        .success(function(moviesObject){
-          Object.keys(moviesObject).forEach(function(key){
-            moviesObject[key].imdbID = key;
-            console.log("key", key);
-            movies.push(moviesObject[key]);
-          });
-          resolve(movies);
-        })
-        .error(function(error){
-          reject(error);
-        })
-    });
-  };
-
-
-
+  return {
+    //Function to grab data from (json) OMDB API
+    getMoviesFromApi : function(){
+      let movies = [];
+      return $q(function(resolve, reject){
+        $http.get(`./data/movies.json`)
+          .success(function(moviesJson){
+            let moviesObject = moviesJson.Search
+            Object.keys(moviesObject).forEach(function(key){
+              // moviesObject[key].imdbID = key;
+              console.log("key", key);
+              movies.push(moviesObject[key]);
+            });
+            resolve(movies);
+          })
+          .error(function(error){
+            reject(error);
+          })
+      });
+    }
 
 
+
+  }
 });
