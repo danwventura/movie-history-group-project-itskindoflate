bdd6ad4fb236efd1bdcd233e1f85db6610678529
Bradley Guthrie <guppy96@gmail.com>
2016-06-04 11:14:34 -0500

    added movie array to hold object items

--- app/controllers/OMDBSearchCtrl.js
+++ app/controllers/OMDBSearchCtrl.js
@@ -3,14 +3,15 @@ app.controller("OMDBSearchCtrl", function($scope, $location, omdbFactory){
 
   // omdbFactory.GetMoviesFromApi().then(function(moviesObject){
   //   console.log("moviesObject from factory promise", moviesObject);
-
+  $scope.movieArray = [];
 
   $scope.dataCheck = function(){
       omdbFactory.getMoviesFromApi().then(function(moviesObject){
-    console.log("moviesObject from factory promise", moviesObject);
+      console.log("moviesObject from factory promise", moviesObject);
 
-    $location.url('#/')
-
+    // $location.url('#/')
+      $scope.movieArray = moviesObject;
+      console.log("Movie Array", $scope.movieArray );
   })
   }
 
